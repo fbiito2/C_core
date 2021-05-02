362ffc7a7d8f479b075d7c7eebd316598ae20464
fbiito2 <32458412+fbiito2@users.noreply.github.com>
2021-05-02 13:03:17 +0800

    u

--- C# 核心課程/Practices/Mod08_1/Solution/Mod08_1/Mod08_1/MainWindow.xaml.cs
+++ C# 核心課程/Practices/Mod08_1/Solution/Mod08_1/Mod08_1/MainWindow.xaml.cs
@@ -16,52 +16,60 @@ using System.Security.Cryptography;
 using System.IO;
 
 
-namespace Mod08_1 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow : Window {
-    Byte[] key;
-    Byte[] iv;
-    Byte[] cipherMessage;
-    string pwd = "Pa$$w0rd";
-    string salt = "S@lt";
+namespace Mod08_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Byte[] key;
+        Byte[] iv;
+        Byte[] cipherMessage;
+        string pwd = "Pa$$w0rd";
+        string salt = "S@lt";
 
-    public MainWindow( ) {
-      InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var alogorithm = new AesManaged();
+            //給明文密碼與sal生出key and iv
+            Rfc2898DeriveBytes rfc = new Rfc2898DeriveBytes(pwd, Encoding.Unicode.GetBytes(salt));
+            key = rfc.GetBytes(alogorithm.KeySize / 8);
+            iv = rfc.GetBytes(alogorithm.BlockSize / 8);
+
+            //也可以不用key and iv的方法
+            ICryptoTransform objCrytpo = alogorithm.CreateEncryptor(key, iv);
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, objCrytpo, CryptoStreamMode.Write);
+            //
+            StreamWriter sw = new StreamWriter(cs);
+            sw.WriteLine(txt.Text);
+            sw.Flush();
+            cs.FlushFinalBlock();
+
+            ms.Position = 0;
+            cipherMessage = new Byte[ms.Length];
+            ms.Read(cipherMessage, 0, (int)ms.Length);
+            tb1.Text = BitConverter.ToString(cipherMessage);
+            cs.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var alogorithm = new AesManaged();
+
+            ICryptoTransform objCrytpo = alogorithm.CreateDecryptor(key, iv);
+            MemoryStream ms = new MemoryStream(cipherMessage);
+            CryptoStream cs = new CryptoStream(ms, objCrytpo, CryptoStreamMode.Read);
+
+            StreamReader sr = new StreamReader(cs);
+            tb2.Text = sr.ReadToEnd();
+            sr.Close();
+        }
     }
-
-    private void Button_Click( object sender, RoutedEventArgs e ) {
-      var alogorithm = new AesManaged();
-      Rfc2898DeriveBytes rfc = new Rfc2898DeriveBytes(pwd, Encoding.Unicode.GetBytes(salt));
-      key = rfc.GetBytes(alogorithm.KeySize / 8);
-      iv = rfc.GetBytes(alogorithm.BlockSize / 8);
-
-      ICryptoTransform objCrytpo = alogorithm.CreateEncryptor(key, iv);
-      MemoryStream ms = new MemoryStream();
-      CryptoStream cs = new CryptoStream(ms, objCrytpo, CryptoStreamMode.Write);
-      StreamWriter sw = new StreamWriter(cs);
-      sw.WriteLine(txt.Text);
-      sw.Flush();
-      cs.FlushFinalBlock();
-
-      ms.Position = 0;
-      cipherMessage = new Byte[ms.Length];
-      ms.Read(cipherMessage, 0, (int) ms.Length);
-      tb1.Text = BitConverter.ToString(cipherMessage);
-      cs.Close();
-    }
-
-    private void Button_Click_1( object sender, RoutedEventArgs e ) {
-      var alogorithm = new AesManaged();
-
-      ICryptoTransform objCrytpo = alogorithm.CreateDecryptor(key, iv);
-      MemoryStream ms = new MemoryStream(cipherMessage);
-      CryptoStream cs = new CryptoStream(ms, objCrytpo, CryptoStreamMode.Read);
-
-      StreamReader sr = new StreamReader(cs);
-      tb2.Text = sr.ReadToEnd();
-      sr.Close();
-    }
-  }
 }
