65de6d72971b5d0f1ce1198f89a806f030853357
fbiito2 <32458412+fbiito2@users.noreply.github.com>
2021-09-24 10:18:16 +0800

    go

--- C# 核心課程/Practices/Mod06_TPL/Program.cs
+++ C# 核心課程/Practices/Mod06_TPL/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            //TaskStart();
+            TaskStart();
 
             //TaskWait();
 
@@ -20,7 +20,7 @@ namespace ConsoleApplication1
 
             //TaskCancel();
 
-            UseParallelInvoke();
+            //UseParallelInvoke();
 
 
             //UseParallelFor();
